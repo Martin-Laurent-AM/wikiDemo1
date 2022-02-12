cfa506b11a8a7b996a72a47ee3c8a43c2586f23f
Edmond-Lau <mt15162295312@gmail.com>
2022-02-12 15:22:40 +0800

    orginal commit

---
+++ src/main/java/com/edmond/WikiDemo1Application.java
@@ -0,0 +1,13 @@
+package com.edmond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikiDemo1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/edmond/WikiDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edmond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
